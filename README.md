55292576d277be0ff8bee1d45d9ae20e20aae481
Leonardo Garcia <lgleogar@gmail.com>
2023-06-30 12:22:17 -0300

    adicionado suporte a csv

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # MMQPY-UFABC
 
-Script porco feito pra calcular a reta de regressão usando a fórmula que os relatórios dos laboratórios da ufabc pedem.
+Script feito pra calcular a reta de regressão usando a fórmula que os relatórios dos laboratórios da ufabc pedem.
 
 Não é bonito, mas funciona.
+
+# Como usar
+
+Só rodar o init.py no terminal. Tem dois modos: ler um valores de um .csv, ou digitar cada valor individualmente.
+Caso queira importar um .csv, crie uma planilha com três colunas: x, y, sigmay. E coloque nelas os valores de x, y, e a incerteza de cada valor de y separados por ponto. Caso esteja usando o Sheets, para mudar os valores de virgula para ponto, é só ir em Arquivo - Configurações e mudar o local de Brasil para Estados Unidos.
+Com o .csv baixado e no diretório do script, é só entrar no terminal "python3 init.py NomeDoArquivoCSV.csv"
+No final, serão mostrados os valores do coeficiente angular e do coeficiente linear, junto com as suas incertezas.
--- init.py
+++ init.py
@@ -1,29 +1,47 @@
 from mmq import mmqpy
+from sys import argv
+
+csv = False
+
+try:
+    if argv[1].endswith(".csv"):
+        csv = argv[1]
+except IndexError:
+    print("Sem CSV. Vai ter que digitar cada valor um por um")
 
 x = []
-print("digite os valores de x ou 'Q' quando terminar: ", end = "")
-while True:
-    inp = input()
-    if inp == 'Q':
-        break
-    else:
-        x.append(float(inp))
-
-count = len(x)
-
 y = []
-print("digite os valores de y: ", end = "")
-for i in range(count):
-    inp = input()
-    y.append(float(inp))
-
 yinc = []
-print("digite a incerteza de cada valor de y: ", end = "")
-for i in range(count):
-    inp = input()
-    yinc.append(float(inp))
+
+if csv:
+    import pandas as pd
+    df = pd.read_csv(csv)
+    x = df["x"]
+    y = df["y"]
+    yinc = df["sigmay"]
+
+else:
+    print("digite os valores de x ou 'Q' quando terminar: ", end = "")
+    while True:
+        inp = input()
+        if inp == 'Q':
+            break
+        else:
+            x.append(float(inp))
+
+    count = len(x)
+
+    print("digite os valores de y: ", end = "")
+    for i in range(count):
+        inp = input()
+        y.append(float(inp))
+
+    print("digite a incerteza de cada valor de y: ", end = "")
+    for i in range(count):
+        inp = input()
+        yinc.append(float(inp))
 
 resultado = mmqpy(x, y, yinc)
 
-print("o valor de a é: "+ str(resultado[0]) + " +- " + str(sresultado[1]))
-print("o valor de b é: " + str(resultado[2]) + " +- " + str(resultado[3]))
+print("o valor do coeficiente angular (a) é: "+ str(resultado[0]) + " +- " + str(resultado[1]))
+print("o valor do coeficiente linear (b) é: " + str(resultado[2]) + " +- " + str(resultado[3]))
